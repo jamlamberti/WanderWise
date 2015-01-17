3370e75f92058e63ecd7e8808feca9a1fc715240
Kurt DaCosta <kurtbradd@gmail.com>
2015-01-17 17:18:28 -0500

    disables landscapre orientation

---
+++ ios/wanderwise/wanderwise/KBAPI.m
@@ -0,0 +1,13 @@
+//
+//  KBAPI.m
+//  wanderwise
+//
+//  Created by Kurt DaCosta on 2015-01-17.
+//  Copyright (c) 2015 kurtbardd. All rights reserved.
+//
+
+#import "KBAPI.h"
+
+@implementation KBAPI
+
+@end
