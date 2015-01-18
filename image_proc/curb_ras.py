36ea28ce22e1d70974d91e6a0e25db01cc44d9a7
James Lamberti <jal416@lehigh.edu>
2015-01-18 00:16:21 -0500

    Some optimized code

--- image_proc/curb_ras.py
+++ image_proc/curb_ras.py
@@ -10,32 +10,24 @@ def process(im, iters=5, smoothLines=True, l_old=None, r_old=None):
     img_raw = im.copy()
     imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 50, 255, cv2.THRESH_BINARY_INV)
-    #cv2.imshow('Binary', thresh)
-    #cv2.waitKey()
     kernel = np.ones((5,5), np.uint8)
     dilation = cv2.dilate(thresh, kernel, iterations=iters)
-    #cv2.imshow('Dilation', dilation)
-    #cv2.waitKey()
     minLength = 300
     conts_fin = []
     ret, thresh = cv2.threshold(dilation, 127, 255, 0)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
     for i in range(len(contours)):
         x, y, w, h = cv2.boundingRect(contours[i])
         if w > minLength or h > minLength:
             conts_fin.append(contours[i])
     
-    cv2.drawContours(img_raw, conts_fin, -1, (0, 255, 0), 3)
+    #cv2.drawContours(img_raw, conts_fin, -1, (0, 255, 0), 3)
     points = []
     for cnt in conts_fin:
         for pnt in cnt:
             points.append(pnt[0])
     x = len(img_raw[0])/2
     y=len(img_raw)
-    cv2.line(img_raw, (x, 0), (x, y), (0, 0, 255), 3)
-    for cnt in contours:
-        cv2.fillPoly(img_raw, cnt, (0, 255, 0))
     w = len(img_raw[0])
     h = len(img_raw)
     boxHeight=150
@@ -49,27 +41,38 @@ def process(im, iters=5, smoothLines=True, l_old=None, r_old=None):
                 leftEdge = pnt[0]
             elif pnt[0] > x and pnt[0] < rightEdge:
                 rightEdge = pnt[0]
-    if l_old is None:
-        cv2.line(img_raw, (leftEdge, 0), (leftEdge, h), (255, 0, 0), thickness=3)
-    else:
-        cv2.line(img_raw, (l_old, 0), (l_old, h), (255, 0, 0), thickness=3)
-    if r_old is None:
-        cv2.line(img_raw, (rightEdge, 0), (rightEdge, h), (255, 0, 0), thickness=3)
-    else:
-        cv2.line(img_raw, (r_old, 0), (r_old, h), (255, 0, 0), thickness=3)
+    left_final = leftEdge
+    right_final = rightEdge
+    if l_old is not None:
+        left_final = l_old
+    if r_old is not None:
+        right_final = r_old
     center = (w/2, h/2)
     M = cv2.getRotationMatrix2D(center, 180, 1.0)
     rotated = cv2.warpAffine(img_raw, M, (w, h))
+    try:
+        average = (right_final + left_final)/2
+        sign = (average - len(img_raw[0])/2)/abs(average - len(img_raw[0])/2)
+        if len(img_raw[0])/2 - average < 5:
+            #cv2.putText(rotated, 'Veere right', (0, h/2), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 0))
+            pass
+        else:
+            #cv2.putText(rotated, 'Veere Left', (0, h/2), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 0))
+            pass
+    except:
+        power = 0
     if smoothLines:
         return rotated, leftEdge, rightEdge
     return rotated
 def test():
     im = cv2.imread('curb1.jpg')
+    im = cv2.resize(im, (0,0), fx=0.5, fy=0.5)
     curb = process(im, smoothLines=False)
     cv2.imwrite('curb1_result.jpg', curb)
     android = ['curb2', 'curb3', 'curb4', 'curb5', 'curb6', 'curb7', 'curb8', 'curb9', 'curb10', 'curb11', 'curb12', 'curb14', 'curb15']
     for i in android:
         im = cv2.imread(i+'.jpg')
+        im = cv2.resize(im, (0,0), fx=0.5, fy=0.5)
         try:
             curb = process(im, smoothLines=False)
             cv2.imwrite(i+'_result.jpg', curb)
---
+++ image_proc/video_process_ras.py
@@ -0,0 +1,37 @@
+import cv2
+from matplotlib import pyplot as plt
+import crosswalk
+import curb
+import time
+vid = cv2.VideoCapture(1)
+vid.set(3, 1280)
+vid.set(1,512)
+time.sleep(2)
+#vid.set(15, -8.0)
+length = 500
+l_sum = [0]
+r_sum = [0]
+while True:
+    success, img = vid.read()
+    img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+    if not success:
+        break
+    w = len(img[0])
+    h = len(img)
+    center = (w/2, h/2)
+    M = cv2.getRotationMatrix2D(center, 180, 1.0)
+    img = cv2.warpAffine(img, M, (w, h))
+    try:
+        img, left, right = curb.process(img, l_old=sum(l_sum)/len(l_sum), r_old=sum(r_sum)/len(r_sum))
+        l_sum.append(left)
+        r_sum.append(right)
+        if len(l_sum) >= 8:
+            del l_sum[0]
+        if len(r_sum) >= 8:
+            del r_sum[0]
+    except:
+        import traceback
+        traceback.print_exc()
+        pass
+    cv2.imshow('My Video', img)
+    cv2.waitKey(10)
