d9e4c59fc686d21f10c9224f8a21244fe9724693
Kurt DaCosta <kurtbradd@gmail.com>
2015-01-18 09:09:23 -0500

    updated backend

--- backend/controllers/DirectionsCtrl.js
+++ backend/controllers/DirectionsCtrl.js
@@ -9,16 +9,24 @@ getDirections = function (req, res) {
 	if (!req.body.destination) return res.send(400, {error: 'Missing Body: Destination'});
 	
 	// use test locations in philly
-	var test_origin = "3643-3661 Walnut St, Philadelphia, PA 19104, USA"
-	var test_destination = "3451 Ludlow St, Philadelphia, PA 19104, USA"
+	// var test_origin = "3643-3661 Walnut St, Philadelphia, PA 19104, USA"
+	// var test_destination = "3451 Ludlow St, Philadelphia, PA 19104, USA"
+	var test_origin = "3312 Walnut St, Philadelphia, PA 19104, USA"
+	var test_destination = "200-238 S 33rd St, Philadelphia, PA 19104, USA"
+
 	var directionsURL = GOOGLE_DIRECTIONS_URL;
 	directionsURL += "&origin=" + test_origin;
 	directionsURL += "&destination=" + test_destination;
 
 	request(directionsURL, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log(body);
-			return res.send(200, JSON.parse(body));
+			var result = JSON.parse(body);
+			var route = result.routes[0]['legs'][0];
+			if (!route) {
+				return res.send(400, {error: "No Directions Found"});
+			}
+			console.log(route);
+			return res.send(200, route);
 		}
 		return res.send(400, error);
 	});
